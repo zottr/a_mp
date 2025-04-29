4a454e2a7c14b27f092a4eb123c99db407fdd66f
Rohit <jshilpirose@gmail.com>
2025-04-29 15:40:32 +0530

    Deployment: Changed subdomain urls

--- src/libs/axios/axiosClient.ts
+++ src/libs/axios/axiosClient.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 // const SERVER_API_URL = 'http://api.zottr.com/server-api';
-const SERVER_API_URL = 'https://api.zottr.com/server-api';
+const SERVER_API_URL = 'https://server-api.zottr.com/';
 // const SERVER_API_URL = 'http://172.18.121.156:3002';
 // const SERVER_API_URL = 'http://localhost:3002';
 
